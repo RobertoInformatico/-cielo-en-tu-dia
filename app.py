9d49206ac1f31ad745ac4f9cfddee7c80c428539
RobertoInformatico <127438281+RobertoInformatico@users.noreply.github.com>
2025-05-23 21:01:46 -0600

    subida 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template_string, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,5 @@ def cielo():
     return render_template_string(HTML, imagen=imagen, titulo=titulo, descripcion=descripcion)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
